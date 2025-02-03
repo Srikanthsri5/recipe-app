8a9dcde951882dc319f375e643ae928dc560d981
Srikanthsri5 <srikanthsri051199@gmail.com>
2025-02-03 12:14:30 +0530

    "modified the docker file for postgres upgrade 3/2/2025"

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+This is a simple module that contains a function that adds two numbers together.
+"""
+
+def add(x,y):
+    """Add two numbers together"""
+    return x + y
+
+def subtract(x,y):
+    """Subtract x from y and return value"""
+    return y - x
