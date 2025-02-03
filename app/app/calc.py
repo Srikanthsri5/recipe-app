be9eff5c64b4769292732f6ae4620462d286d11d
Srikanthsri5 <srikanthsri051199@gmail.com>
2025-02-03 21:01:36 +0530

    modified wait_for_db file 03-02-2025

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,14 @@
 """
-This is a simple module that contains a function that adds two numbers together.
+This is a simple module that contains a function
+that adds two numbers together.
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add two numbers together"""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """Subtract x from y and return value"""
     return y - x
