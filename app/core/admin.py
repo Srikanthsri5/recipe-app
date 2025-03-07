8591744877c68fa574ecddf1f196875858ecaf67
Srikanthsri5 <srikanthsri051199@gmail.com>
2025-03-07 16:51:46 +0530

    created a user api and added tests 07-03-2025

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,44 @@
-from django.contrib import admin # noqa
+"""
+Django admin configuration for the core app.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext as _
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Admin pages for users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        # ('Personal Info', {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active', 
+                    'is_staff', 
+                    'is_superuser'
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 
+                'password1', 
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
