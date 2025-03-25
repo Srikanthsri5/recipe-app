040542c1747ee96e461b9641089d56c69271b936
Srikanthsri5 <srikanthsri051199@gmail.com>
2025-03-25 19:06:26 +0530

    Add Tag model and associate with Recipe; update admin and tests 20-3-2025

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,6 +8,9 @@ from django.contrib.auth import get_user_model # type: ignore
 
 from core import models
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
 
 class ModelTests(TestCase):
     """Test models."""
@@ -64,3 +67,9 @@ class ModelTests(TestCase):
             description='Sample recipe description'
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a new tag"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+        self.assertEqual(str(tag), tag.name)
