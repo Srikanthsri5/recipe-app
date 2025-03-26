aefea8b13449d81f4ce3e26777d6345728be8d9f
Srikanthsri5 <srikanthsri051199@gmail.com>
2025-03-26 22:22:19 +0530

    Add Ingredient model and API; update serializers, views, and tests 26-03-2025

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -2,19 +2,22 @@
 Urls for recipe api
 """
 
-from django.urls import (
+from django.urls import ( # type: ignore
     path, 
     include
-)
-from rest_framework.routers import DefaultRouter
+) 
+from rest_framework.routers import DefaultRouter # type: ignore
 from recipe import views
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 app_name = 'recipe'
 
 urlpatterns = [
     path('', include(router.urls))
+
 
 ]
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,12 +1,14 @@
 """
 Views for the recipe api
 """
-from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets # type: ignore
+from rest_framework.authentication import TokenAuthentication # type: ignore 
+from rest_framework.permissions import IsAuthenticated # type: ignore
 
-from core.models import Recipe
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
+
+from rest_framework import viewsets, mixins # type: ignore
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """Manage recipes in the database"""
@@ -29,3 +31,32 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
+
+class TagViewSet(viewsets.GenericViewSet, 
+                 mixins.ListModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.DestroyModelMixin):
+    """Manage tags in the database"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        """Return tags for the current authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    # def perform_create(self, serializer):
+    #     """Create a new tag"""
+    #     serializer.save(user=self.request.user)
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in DB"""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
